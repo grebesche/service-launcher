b7c4753f9e4c31c3918a872ce987cf3ed9259e8a
grebesche <guillaume.rebesche@gmail.com>
2016-08-01 19:56:34 -0400

    Moved some code to the MainUI

--- src/com/grebesche/servicelauncher/Main.java
+++ src/com/grebesche/servicelauncher/Main.java
@@ -1,84 +1,19 @@
 package com.grebesche.servicelauncher;
 
-import com.grebesche.servicelauncher.model.ApplicationModel;
-import com.grebesche.servicelauncher.model.Service;
-import com.grebesche.servicelauncher.ui.ServiceEditorUI;
-import com.grebesche.servicelauncher.ui.ServiceUI;
+import com.grebesche.servicelauncher.ui.MainUI;
 import javafx.application.Application;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 public class Main extends Application {
 
-  private ApplicationModel applicationModel;
-  private ServiceEditorUI serviceEditorUI;
-  private VBox mainLayout;
-  private Map<String, ServiceUI> servicesUIMap = new HashMap<>();
+  private MainUI mainUI;
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-
-    serviceEditorUI = new ServiceEditorUI(primaryStage);
-    applicationModel = new ApplicationModel();
-    serviceEditorUI.setEditedServiceCallback(this::applyEditedService);
-    mainLayout = new VBox();
-
-    Button addServiceButton = new Button("add service");
-    addServiceButton.setOnMouseClicked(event -> {
-      serviceEditorUI.show(null);
-    });
-    mainLayout.getChildren().add(addServiceButton);
-
-    primaryStage.setTitle("Service launcher");
-    primaryStage.setScene(new Scene(mainLayout, 300, 275));
-    primaryStage.show();
+    mainUI = new MainUI();
+    mainUI.start(primaryStage);
   }
 
-  private void applyEditedService(Service service) {
-    int indexOf = applicationModel.getServices().indexOf(service);
-    if (indexOf != -1) {
-      applicationModel.getServices().set(indexOf, service);
-      ServiceUI serviceUI = servicesUIMap.get(service.getId());
-      serviceUI.setService(service);
-    } else {
-      applicationModel.getServices().add(service);
-      ServiceUI serviceUI = new ServiceUI();
-      serviceUI.setService(service);
-      serviceUI.setStartServiceExecutor(() -> startService(service));
-      serviceUI.setEditServiceExecutor(() -> editService(service));
-      serviceUI.setDeleteServiceExecutor(() -> deleteService(service));
-      servicesUIMap.put(service.getId(), serviceUI);
-      mainLayout.getChildren().add(serviceUI.getContainer());
-    }
-  }
-
-  private void deleteService(Service service) {
-    ServiceUI serviceUI = servicesUIMap.remove(service.getId());
-    Iterator<Node> iterator = mainLayout.getChildren().iterator();
-    while (iterator.hasNext()) {
-      Node node = iterator.next();
-      if(node.equals(serviceUI.getContainer())) {
-        iterator.remove();
-        break;
-      }
-    }
-    applicationModel.getServices().remove(service);
-  }
-
-  private void editService(Service service) {
-    serviceEditorUI.show(service);
-  }
-
-  public void startService(Service service) {
-
-  }
 
   public static void main(String[] args) {
     launch(args);
